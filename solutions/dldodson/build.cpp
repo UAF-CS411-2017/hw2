1c811d8027125d47033362bda8aaa119a29998b8
Dustin L. Dodson <dustinleedodson@gmail.com>
2017-09-22 15:11:22 -0800

    Added some basic things to get it to compile.

---
+++ solutions/dldodson/build.cpp
@@ -0,0 +1,24 @@
+#ifndef _CS_411_HW2_CPP_
+#define _CS_411_HW2_CPP_
+
+
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout;
+using std::endl;
+
+const int build(const int & w, const int & e, const vector<vector<int>> & cbs) {
+	cout << "W: " << w << endl;
+	cout << "E: " << e << endl;
+	for(auto i = 0; i < cbs.size(); i ++) {
+		for(auto j = 0; j < cbs[i].size(); j++) {
+			cout << cbs[i][j] << " ";
+		}
+		cout << endl;
+	}
+	// exit(0);
+	return 0;
+}
+
+#endif
---
+++ solutions/dldodson/build.h
@@ -0,0 +1,6 @@
+#ifndef _CS_411_HW2_H_
+#define _CS_411_HW2_H_
+
+
+
+#endif
