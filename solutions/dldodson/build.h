9d3e20990060c0cc66e50ef5bafb05540e8010a6
Dustin L. Dodson <dustinleedodson@gmail.com>
2017-09-26 17:03:57 -0800

    Cleaned up quite a bit, got rid of Bridge objects.

--- solutions/dldodson/build.h
+++ solutions/dldodson/build.h
@@ -3,101 +3,21 @@
 
 #include <vector>
 using std::vector;
-#include <iostream>
-using std::cout;
-using std::endl;
-#include <sstream>
-using std::ostringstream;
-#include <string>
-using std::string;
 
-struct Bridge {
-	Bridge(int n, int w, int e, int we): 
-		name(n), west(w), east(e), weight(we) {}
-
-	const string toString() {
-		ostringstream out;
-		out << this->west << ' ' << this->east << ' ' << this->weight << endl;
-		return out.str();
-	}
-	static const string toString(const vector<Bridge> & bridges) {
-		ostringstream out;
-		for(auto bridge : bridges) {
-			out << bridge.name << ' ';
-		}
-
-		return out.str();
-	}
-
-	const bool crosses(const Bridge & other) const {
-		return (this->west < other.west && this->east > other.east) || (this->west > other.west && this->east < other.east);
-	}
-
-	const bool crosses(const vector<Bridge> & bridges) const {
-		for(auto bridge : bridges)
-			if(this->crosses(bridge))
-				return true;
-
-		return false;
-	}
-
-	const bool connectsToSameCity(const Bridge & other) const {
-		return (this->west == other.west || this->east == other.east);
-	}
-
-	const bool connectsToSameCity(const vector<Bridge> & bridges) const {
-		for(auto bridge : bridges)
-			if(this->connectsToSameCity(bridge))
-				return true;
-
-		return false;
-	}
-
-	const bool operator==(const Bridge & rhs) const {
-		bool westEquals, eastEquals, weightEquals;
-
-		westEquals = (this->west == rhs.west);
-		eastEquals = (this->east == rhs.east);
-		weightEquals = (this->weight == rhs.weight);
-
-		return (westEquals && eastEquals && weightEquals);
-	}
-
-	const bool equals(const Bridge & rhs) const {
-		bool westEquals, eastEquals, weightEquals;
-
-		westEquals = (this->west == rhs.west);
-		eastEquals = (this->east == rhs.east);
-		weightEquals = (this->weight == rhs.weight);
-
-		return (westEquals && eastEquals && weightEquals);
-	}
-
-	int name;
-	int west;
-	int east;
-	int weight;
-};
-
-const unsigned int addWeights(const vector<Bridge> & bridges) {
-	unsigned int weight = 0;
-
-	for(auto bridge : bridges)
-		weight += bridge.weight;
-
-	return weight;
-}
-
-const unsigned int getMax(const vector<Bridge> & initialSet, vector<Bridge> & currentSet, const unsigned int index) {
-	unsigned int max = 0;
-
+const unsigned int getMax(const vector<vector<int>> & initialSet, vector<vector<int>> & currentSet, const unsigned int index) {
 	if(index < initialSet.size()) {
 		auto item = initialSet[index];
+		auto valid = true;
 
-		if(item.crosses(currentSet) || item.connectsToSameCity(currentSet)) {
-			return getMax(initialSet, currentSet, index + 1);
+		for(auto curr : currentSet) {
+			if((item[0] < curr[0] && item[1] > curr[1]) || (item[0] > curr[0] && item[1] < curr[1]))
+				valid = false;
+
+			if(item[0] == curr[0] || item[1] == curr[1])
+				valid = false;
 		}
-		else {
+
+		if(valid) {
 			currentSet.push_back(item);
 			auto withMe = getMax(initialSet, currentSet, index + 1);
 			currentSet.pop_back();
@@ -105,18 +25,24 @@ const unsigned int getMax(const vector<Bridge> & initialSet, vector<Bridge> & cu
 
 			return (withMe > withoutMe ? withMe:withoutMe);
 		}
+		else {
+			return getMax(initialSet, currentSet, index + 1);
+		}
 	}
-	else return addWeights(currentSet);
+	else {
+		unsigned int max = 0;
+		
+		for(auto item : currentSet)
+			max += item[2];
+
+		return max;
+	}
 }
 
 const int build(const int & w, const int & e, const vector<vector<int>> & cbs) {
-	vector<Bridge> bridges, workingSet;
-	for(auto i = 0; i < cbs.size(); i ++) {
-		Bridge newBridge(i, cbs[i][0], cbs[i][1], cbs[i][2]);
-		bridges.push_back(newBridge);
-	}
+	vector<vector<int>> workingSet;
 
-	return getMax(bridges, workingSet, 0);
+	return getMax(cbs, workingSet, 0);
 }
 
 #endif
