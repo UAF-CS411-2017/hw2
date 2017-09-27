ec763a37e8467a22ef6d6cdfc0a1b6a365039875
Dustin L. Dodson <dustinleedodson@gmail.com>
2017-09-26 17:21:16 -0800

    Moved code to .cpp and left function prototypes in .h

--- solutions/dldodson/build.cpp
+++ solutions/dldodson/build.cpp
@@ -1,24 +1,43 @@
 #ifndef _CS_411_HW2_CPP_
 #define _CS_411_HW2_CPP_
 
+#include "build.h"
 
-// #include <vector>
-// using std::vector;
-// #include <iostream>
-// using std::cout;
-// using std::endl;
+const unsigned int getMax(const vector<vector<int>> & initialSet, vector<vector<int>> & currentSet, const unsigned int index) {
+	if(index < initialSet.size()) {
+		auto item = initialSet[index];
+		auto valid = true;
 
-// const int build(const int & w, const int & e, const vector<vector<int>> & cbs) {
-// 	cout << "W: " << w << endl;
-// 	cout << "E: " << e << endl;
-// 	for(auto i = 0; i < cbs.size(); i ++) {
-// 		for(auto j = 0; j < cbs[i].size(); j++) {
-// 			cout << cbs[i][j] << " ";
-// 		}
-// 		cout << endl;
-// 	}
-// 	// exit(0);
-// 	return 0;
-// }
+		for(auto curr : currentSet)
+			if((item[0] <= curr[0] && item[1] >= curr[1]) || (item[0] >= curr[0] && item[1] <= curr[1]))
+				valid = false;
+
+		if(valid) {
+			currentSet.push_back(item);
+			auto withMe = getMax(initialSet, currentSet, index + 1);
+			currentSet.pop_back();
+			auto withoutMe = getMax(initialSet, currentSet, index + 1);
+
+			return (withMe > withoutMe ? withMe:withoutMe);
+		}
+		else {
+			return getMax(initialSet, currentSet, index + 1);
+		}
+	}
+	else {
+		unsigned int max = 0;
+		
+		for(auto item : currentSet)
+			max += item[2];
+
+		return max;
+	}
+}
+
+const int build(const int & w, const int & e, const vector<vector<int>> & cbs) {
+	vector<vector<int>> workingSet;
+
+	return getMax(cbs, workingSet, 0);
+}
 
 #endif
--- solutions/dldodson/build.h
+++ solutions/dldodson/build.h
@@ -4,41 +4,7 @@
 #include <vector>
 using std::vector;
 
-const unsigned int getMax(const vector<vector<int>> & initialSet, vector<vector<int>> & currentSet, const unsigned int index) {
-	if(index < initialSet.size()) {
-		auto item = initialSet[index];
-		auto valid = true;
-
-		for(auto curr : currentSet)
-			if((item[0] <= curr[0] && item[1] >= curr[1]) || (item[0] >= curr[0] && item[1] <= curr[1]))
-				valid = false;
-
-		if(valid) {
-			currentSet.push_back(item);
-			auto withMe = getMax(initialSet, currentSet, index + 1);
-			currentSet.pop_back();
-			auto withoutMe = getMax(initialSet, currentSet, index + 1);
-
-			return (withMe > withoutMe ? withMe:withoutMe);
-		}
-		else {
-			return getMax(initialSet, currentSet, index + 1);
-		}
-	}
-	else {
-		unsigned int max = 0;
-		
-		for(auto item : currentSet)
-			max += item[2];
-
-		return max;
-	}
-}
-
-const int build(const int & w, const int & e, const vector<vector<int>> & cbs) {
-	vector<vector<int>> workingSet;
-
-	return getMax(cbs, workingSet, 0);
-}
+const unsigned int getMax(const vector<vector<int>> & initialSet, vector<vector<int>> & currentSet, const unsigned int index);
+const int build(const int & w, const int & e, const vector<vector<int>> & cbs);
 
 #endif
